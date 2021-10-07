f7acf778106af7c98bef260612298d35593d4e94
Adriano Raiano <adriano@raiano.ch>
2021-10-07 18:26:44 +0200

    fix some paths

--- src/data/marketing/TestimonialSliderData.js
+++ src/data/marketing/TestimonialSliderData.js
@@ -1,25 +1,25 @@
 // import Images from "~data/imageImports"
 const Testimonialdata = [
     {
-        image:"image/marketing/user-1.png",
-        text:"Simply the best. Better than all the rest. I’d recommend their service to beginners and advanced users.",
-        name:"Mariano Rasgado",
-        position:"Founder",
-        reviewStar:5,
+        image: "/image/marketing/user-1.png",
+        text: "Simply the best. Better than all the rest. I’d recommend their service to beginners and advanced users.",
+        name: "Mariano Rasgado",
+        position: "Founder",
+        reviewStar: 5,
     },
     {
-        image:"image/marketing/user-2.png",
-        text:"Simply the best. Better than all the rest. I’d recommend their service to beginners and advanced users.",
-        name:"Mariano Rasgado",
-        position:"Founder",
-        reviewStar:3,
+        image: "/image/marketing/user-2.png",
+        text: "Simply the best. Better than all the rest. I’d recommend their service to beginners and advanced users.",
+        name: "Mariano Rasgado",
+        position: "Founder",
+        reviewStar: 3,
     },
     {
-        image:"image/marketing/user-3.png",
-        text:"Simply the best. Better than all the rest. I’d recommend their service to beginners and advanced users.",
-        name:"Mariano Rasgado",
-        position:"Founder",
-        reviewStar:2,
+        image: "/image/marketing/user-3.png",
+        text: "Simply the best. Better than all the rest. I’d recommend their service to beginners and advanced users.",
+        name: "Mariano Rasgado",
+        position: "Founder",
+        reviewStar: 2,
     }
 ]
 
--- src/data/project/features.js
+++ src/data/project/features.js
@@ -4,16 +4,16 @@ const featureSectionData = {
     featureTabTitle3:"Make Schedules<br className='d-none d-xs-block' />ffor Any Meeting<br className='d-none d-xs-block' /> on Calendar.",
     tabWidget1: [
         {
-            icon:"image/project-management/icon-grid.png",
-            iconBackground:"#ff8d52",
-            title:"Manage Smartly",
-            text:"Stay on top of your task lists and stay <br class='d-none d-xs-block'> in touch with what's happening",
+            icon: "/image/project-management/icon-grid.png",
+            iconBackground: "#ff8d52",
+            title: "Manage Smartly",
+            text: "Stay on top of your task lists and stay <br class='d-none d-xs-block'> in touch with what's happening",
         },
         {
-            icon:"image/project-management/icon-message.png",
-            iconBackground:"#15cda8",
-            title:"Communicate Fast",
-            text:"Stay on top of your task lists and stay <br class='d-none d-xs-block'> in touch with what's happening",
+            icon: "/image/project-management/icon-message.png",
+            iconBackground: "#15cda8",
+            title: "Communicate Fast",
+            text: "Stay on top of your task lists and stay <br class='d-none d-xs-block'> in touch with what's happening",
         },
     ],
     
