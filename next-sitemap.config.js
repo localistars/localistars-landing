9fa907f3bdb61727f14277db9ac93a1f467d9fd0
Sandro Huber <sandro@sh-digital.ch>
2021-11-22 10:34:28 +0100

    sitemap, robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  changefreq: 'daily',
+  priority: 1.0,
+  generateRobotsTxt: true, // (optional)
+  exclude: ['/dpa', '/ccpa']
+  alternateRefs: [
+    {
+      href: process.env.SITE_URL + '/en',
+      hreflang: 'en',
+    },
+    {
+      href: process.env.SITE_URL + '/de',
+      hreflang: 'de',
+    },
+  ],
+}
