0883a43aa016c0bfd659026307ec005a070fc5ca
Adriano Raiano <adriano@raiano.ch>
2021-11-30 09:38:51 +0100

    remove debug file

--- modifySitemapMod.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-let content = fs.readFileSync(path.join(__dirname, 'node_modules/next-sitemap/dist/cjs/file/index.js')).toString()
-
-content = content.replace('fs_1.default.writeFileSync(filePath, content);', `fs_1.default.writeFileSync(filePath, content);
-    console.log(filePath);`)
-
-fs.writeFileSync(path.join(__dirname, 'node_modules/next-sitemap/dist/cjs/file/index.js'), content)
